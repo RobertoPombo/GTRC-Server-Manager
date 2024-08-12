8feafa352bd46d04a2f241085c6d5aeea4853e41
RobertoPombo <captn.sponge2@web.de>
2024-08-12 14:30:53 +0200

    Added MainWindow, MainVM, ResourceDictionary, GTRC_WPF_UserControls.

---
+++ ViewModels/MainVM.cs
@@ -0,0 +1,23 @@
+﻿using GTRC_WPF_UserControls.ViewModels;
+
+namespace GTRC_Server_Bot.ViewModels
+{
+    public class MainVM : GTRC_WPF.ViewModels.MainVM
+    {
+        public static MainVM? Instance;
+
+        private DbApiConnectionConfigVM? dbApiConnectionConfigVM;
+        private DiscordBotConfigVM? discordBotConfigVM;
+
+        public MainVM()
+        {
+            Instance = this;
+            DbApiConnectionConfigVM = new DbApiConnectionConfigVM();
+            DiscordBotConfigVM = new DiscordBotConfigVM();
+        }
+
+        public DbApiConnectionConfigVM? DbApiConnectionConfigVM { get { return dbApiConnectionConfigVM; } set { dbApiConnectionConfigVM = value; RaisePropertyChanged(); } }
+
+        public DiscordBotConfigVM? DiscordBotConfigVM { get { return discordBotConfigVM; } set { discordBotConfigVM = value; RaisePropertyChanged(); } }
+    }
+}
--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,4 +1,14 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+
+using GTRC_Basics;
+using GTRC_Database_Client.Responses;
+using GTRC_Database_Client;
+using GTRC_WPF;
+using GTRC_WPF_UserControls.Scripts;
+using GTRC_WPF_UserControls.ViewModels;
 
 namespace GTRC_Server_Bot.Windows
 {
@@ -6,7 +16,34 @@ namespace GTRC_Server_Bot.Windows
     {
         public MainWindow()
         {
+            if (!Directory.Exists(GlobalValues.DataDirectory)) { Directory.CreateDirectory(GlobalValues.DataDirectory); }
+            GlobalWinValues.SetCultureInfo();
+            UpdateThemeColors();
+            DiscordCommandsBase.DiscordBot.EnsureIsRunning();
             InitializeComponent();
+            Width = GlobalWinValues.screenWidth * 0.5;
+            Height = GlobalWinValues.screenHeight * 0.8;
+            Left = ((GlobalWinValues.screenWidth / 2) - (Width / 2)) * 1;
+            Top = ((GlobalWinValues.screenHeight / 2) - (Height / 2)) * 1;
+            Closing += CloseWindow;
+            DbApiConnectionConfigVM.ConfirmApiConnectionEstablished += UpdateThemeColors;
+        }
+
+        public void CloseWindow(object? sender, CancelEventArgs e) { }
+
+        public void UpdateThemeColors() { _ = UpdateThemeColorsAsync(); }
+
+        public async Task UpdateThemeColorsAsync()
+        {
+            DbApiListResponse<GTRC_Basics.Models.Color> response = await DbApi.DynConnection.Color.GetAll();
+            List<GTRC_Basics.Models.Color> colors = response.List;
+            for (int colorNr = 0; colorNr < colors.Count; colorNr++)
+            {
+                SolidColorBrush _color = new(Color.FromArgb(colors[colorNr].Alpha, colors[colorNr].Red, colors[colorNr].Green, colors[colorNr].Blue));
+                if (colorNr < WpfColors.List.Count) { WpfColors.List[colorNr] = _color; }
+                else { WpfColors.List.Add(_color); }
+            }
+            GlobalWinValues.UpdateWpfColors(this);
         }
     }
 }
